c07bf47958100f9cc26c95d2f2a143990b05fdb2
dineug <dineug2@gmail.com>
2021-01-16 16:57:38 +0900

    fix: typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,11 @@
 import { TemplateResult, SVGTemplateResult } from "lit-html";
 
+type Callback = () => void;
 type Template = () => TemplateResult | SVGTemplateResult;
-
+type PropName = string | number | symbol;
+type Observer = () => void;
+type SubjectObserver<T> = (value: T) => void;
+export type Unsubscribe = () => void;
 export type FunctionalComponent<P = any, T = HTMLElement> = (
   this: HTMLElement,
   props: P,
@@ -11,28 +15,28 @@ export type FunctionalComponent<P = any, T = HTMLElement> = (
 interface ShadowOptions {
   mode: "open" | "closed";
 }
-
 interface Options {
   observedProps?: string[];
   shadow?: ShadowOptions;
   style?: string;
   render: FunctionalComponent<any, any>;
 }
-
-declare function defineComponent(name: string, options: Options): void;
-
-declare function closestElement(selector: string, el: any): Element | null;
-
-type PropName = string | number | symbol;
-type Observer = () => void;
-export type Unsubscribe = () => void;
-type SubjectObserver<T> = (value: T) => void;
-
+interface Ref<T> {
+  value: T;
+}
 export interface Subject<T> {
   next(value: T): void;
   subscribe(observer: SubjectObserver<T>): Unsubscribe;
 }
 
+declare function beforeMount(callback: Callback): void;
+declare function mounted(callback: Callback): void;
+declare function unmounted(callback: Callback): void;
+declare function updated(callback: Callback): void;
+declare function query<T = any>(selector: string): Ref<T>;
+declare function queryAll<T = any>(selector: string): Ref<T>;
+declare function defineComponent(name: string, options: Options): void;
+declare function closestElement(selector: string, el: any): Element | null;
 declare function observable<T>(raw: T): T;
 declare function observer(f: Observer): Unsubscribe;
 declare function createSubject<T>(): Subject<T>;
