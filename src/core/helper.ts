ededf21ca16a71a6e573563c9e264793d2f0f44c
dineug <dineug2@gmail.com>
2021-02-22 23:35:53 +0900

    fix: queryShadowSelector, queryShadowSelectorAll

--- src/core/helper.ts
+++ src/core/helper.ts
@@ -12,55 +12,26 @@ export const closestElement = (
     ? target
     : closestElement(selector, el.getRootNode().host);
 
-function querySelector(
-  selector: string,
-  el: Element | null | undefined
-): Element | null | undefined {
-  const target = el?.querySelector(selector);
-  return target ? target : el?.shadowRoot?.querySelector(selector);
-}
+export const queryShadowSelector = (selectors: string[], el: Element) =>
+  selectors.length
+    ? selectors.reduce<Element | null | undefined>((element, selector) => {
+        const target = element?.querySelector(selector);
+        return target ? target : element?.shadowRoot?.querySelector(selector);
+      }, el)
+    : null;
 
-function querySelectorAll(selector: string, elements: Element[]): Element[] {
-  const target: Element[] = [];
-  elements.forEach(element => {
-    element.shadowRoot &&
-      target.push(...element.shadowRoot.querySelectorAll(selector));
-    target.push(...element.querySelectorAll(selector));
-  });
-  return target;
-}
-
-export function queryShadowSelector(
-  selectors: string[],
-  el: Element
-): Element | null | undefined {
-  if (!selectors.length) return;
-  else if (selectors.length === 1) return querySelector(selectors[0], el);
-
-  const nextSelectors = selectors.slice(0, selectors.length - 1);
-  const lastSelector = selectors[selectors.length - 1];
-  const lastElement = nextSelectors.reduce<Element | null | undefined>(
-    (nextElement, nextSelector) => querySelector(nextSelector, nextElement),
-    el
-  );
-
-  return querySelector(lastSelector, lastElement);
-}
-
-export function queryShadowSelectorAll(
-  selectors: string[],
-  el: Element
-): Element[] {
-  if (!selectors.length) return [];
-  else if (selectors.length === 1) return querySelectorAll(selectors[0], [el]);
-
-  const nextSelectors = selectors.slice(0, selectors.length - 1);
-  const lastSelector = selectors[selectors.length - 1];
-  const lastElements = nextSelectors.reduce<Array<Element>>(
-    (nextElements, nextSelector) =>
-      querySelectorAll(nextSelector, nextElements),
-    [el]
-  );
-
-  return querySelectorAll(lastSelector, lastElements);
-}
+export const queryShadowSelectorAll = (selectors: string[], el: Element) =>
+  selectors.length
+    ? selectors.reduce<Array<Element>>(
+        (elements, selector) => {
+          const target: Element[] = [];
+          elements.forEach(element => {
+            element.shadowRoot &&
+              target.push(...element.shadowRoot.querySelectorAll(selector));
+            target.push(...element.querySelectorAll(selector));
+          });
+          return target;
+        },
+        [el]
+      )
+    : [];
